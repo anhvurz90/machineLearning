da08fa242a8a03c121c8c0dc1e654c3d0dd244e6
anhvurz90 <anhvurz90@gmail.com>
2017-05-19 16:33:56 +0700

    Add Lecture 01:0->45m

---
+++ Stanford/courses/css229/lectures.java
@@ -0,0 +1,22 @@
+Lecturer: Prof Andrew Ng.
+01. {
+	- Tools: Mathlab, Octave
+	01.01.Machine Learning Definition: {
+		- Arthur Samuel (1959): ML: Field of study that gives
+		computers the ability to learn without being 
+		explicitly programmed.
+			+ Checker program
+		- Tom Mitchell (1998): Well-posed Learning Problem:
+		A computer program is said to learn from experience
+		E with respect to some task T and some performance 
+		measure P, if its performance on T, as measured by P,
+		improves with experience E.
+	}
+	01.02.4 Big Topis: {
+		- Supervised Learning:
+			+ House footage/$
+			+ Classification 
+				* Malignant/tumor
+				* Malignant/age, tumor size
+	}
+}
